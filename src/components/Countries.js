daa70a3b3c3198cf3343d2e202bacaa2efafb824
DosSantosBrunoo <bruno.dossantos@anima.edu.uy>
2021-08-05 20:38:20 -0300

    front

---
+++ src/components/Countries.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+
+export default class Countries extends React.Component {
+  state = {
+    countries: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://restcountries.eu/rest/v2/all?fields=name`)
+      .then((res) => {
+        const countries = res.data;
+        this.setState({ countries });
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <select>
+          {this.state.countries.map((countries) => (
+            <option>{countries.name}</option>
+          ))}
+        </select>
+      </>
+    );
+  }
+}
